93b71e0b9e28290a92b905de57853c316b641d56
mani5140 <srimanikantagarapati12@gmail.com>
2024-10-01 18:26:11 +0530

    added svg image

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Dashboard from "./pages/Dashboard";
 import Landing from "./Components/Layout/Landing";
+import Demo from "./Components/Layout/demo";
 
 const App = () => {
   return (
     <main className="h-full">
-      <Landing/>
+      {/* <Landing/> */}
+      <Demo />
       {/* <Dashboard/> */}
     </main>
   );
---
+++ src/Components/Layout/demo.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+import './Demo.css';
+import Landing from './Landing';
+
+function Demo() {
+  useEffect(() => {
+    const light = document.querySelector('.light');
+    const grid = document.querySelector('#hex-grid');
+
+    const handleMouseMove = (e) => {
+      light.style.left = `${e.clientX}px`;
+      light.style.top = `${e.clientY}px`;
+    };
+
+    grid.addEventListener('mousemove', handleMouseMove);
+    return () => {
+      grid.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
+  return (
+    <div className="container">
+      <header id="hex-grid">
+        <div className="light"></div>
+        <div className="grid"></div>
+        
+      </header>
+      <Landing />
+    </div>
+  );
+}
+
+export default Demo;
